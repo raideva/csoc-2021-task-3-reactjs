cd9d7180d2f7df144365ae36ad93338a177fba55
raideva <85274247+raideva@users.noreply.github.com>
2021-06-28 23:13:20 +0530

    Update no_auth_required.js

--- middlewares/no_auth_required.js
+++ middlewares/no_auth_required.js
@@ -1,3 +1,14 @@
 /***
- * @todo Redirect the user to main page if token is present.
+ * @todo Redirect the user to login page if token is not present.
  */
+ import Router from "next/router";
+ import { useContext, useEffect, createContext } from "react";
+
+
+ export function NoAuthRequired() {
+    useEffect(() => {
+        if (localStorage.token) {
+            Router.replace("/");
+        }
+    });
+}
