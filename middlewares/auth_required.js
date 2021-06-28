1851108025512fc92bad99bc9c7c9e80cded9384
raideva <85274247+raideva@users.noreply.github.com>
2021-06-28 23:12:29 +0530

    Update auth_required.js

--- middlewares/auth_required.js
+++ middlewares/auth_required.js
@@ -1,3 +1,14 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+ import Router from "next/router";
+ import { useContext, useEffect, createContext } from "react";
+
+
+ export function useAuthRequired() {
+    useEffect(() => {
+        if (!localStorage.token) {
+            Router.replace("/login/");
+        }
+    });
+}
